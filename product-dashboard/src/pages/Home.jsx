fb2efff56e53e3c1411bca9732ff47ecde31a14b
yashdark01 <137425534+yashdark01@users.noreply.github.com>
2025-02-09 14:36:20 +0530

    added history

--- product-dashboard/src/pages/Home.jsx
+++ product-dashboard/src/pages/Home.jsx
@@ -206,7 +206,7 @@ const Home = () => {
         {filteredProducts.length === 0 ? (
           <ProductNotFound />
         ) : (
-          <div className="w-full h-auto grid grid-cols-1 ssm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3  gap-6">
+          <div className="w-full h-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4  gap-6">
             {filteredProducts.map((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
